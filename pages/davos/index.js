3898b5aebb7176476c80fc3994ecabecc5add4bc
David Agaybi <davidagaybi@gmail.com>
2021-10-10 00:49:03 -0400

    fourth commit added json  data and fetch

--- pages/davos/index.js
+++ pages/davos/index.js
@@ -1,7 +1,26 @@
-const Davos = () => {
+import styles from '../../styles/Davos.module.css';
+
+export const getStaticProps = async () => {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json();
+
+    return {
+        props: {davos: data}
+    }
+}
+
+const Davos = ({davos}) => {
     return (
         <div>
             <h1>All Davos</h1>
+            {davos.map(davo => (
+                <div key={davo.id}>
+                    <a className={styles.single}>
+                        <h3>{davo.name}</h3>
+                    </a>
+                </div>
+            ))}
         </div>
     );
 }
