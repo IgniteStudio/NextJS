ffabd3f3014363ce5dc3385184e5ff0d52259a4e
David Agaybi <davidagaybi@gmail.com>
2021-10-09 21:58:31 -0400

    first commit davo list next js

---
+++ comps/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copyright 2021 Davos List</p>
+        </footer>
+            
+        
+    );
+}
+ 
+export default Footer;
---
+++ pages/davos/index.js
@@ -0,0 +1,9 @@
+const Davos = () => {
+    return (
+        <div>
+            <h1>All Davos</h1>
+        </div>
+    );
+}
+ 
+export default Davos;
